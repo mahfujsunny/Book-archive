966f2c6ed05418f4d46062534c5e5aaa8daea5cd
mahfujsunny <mahfujsunny.info@gmail.com>
2021-09-02 17:22:33 +0600

    Final

--- JS/app.js
+++ JS/app.js
@@ -10,17 +10,30 @@ const bookCover = document.getElementById('book-details');
 // Error Handle div
 const errorHandle = document.getElementById('error-handle');
 
+// Result handle div
+const resultFound = document.getElementById('result-found');
+
+// spinner
+const toggleSpinner = displayStyle => {
+    document.getElementById('loading-spinner').style.display = displayStyle;
+};
+
+// search Button
 searchBtn.addEventListener('click', ()=>{
     const searchText = inputFeild.value;
-    
 
+    // spinner start
+    toggleSpinner('block');
+    
+    // Error Handle 
     if(inputFeild.value === ''){
+
         // clear error 
         errorHandle.textContent = '';
 
         const errorPara = document.createElement('p');
         errorPara.innerHTML = `
-            <h3>Input field can't be empty</h3>
+            <h4 class="text-white text-center bg-danger mx-auto rounded p-3 w-50">Input field can't be empty</h4>
         `;
         errorHandle.appendChild(errorPara);
 
@@ -29,36 +42,56 @@ searchBtn.addEventListener('click', ()=>{
 
         // clear cover showing
         bookCover.textContent = '';
+
+        // clear result showing
+        resultFound.textContent = '';
+
+        // spinner stop
+        toggleSpinner('none');
         return;
     }
     // clear error
     errorHandle.textContent = '';
     
-    const bookUrl = `http://openlibrary.org/search.json?q=${searchText}`;
+    // book search by name API
+    const bookUrl = `https://openlibrary.org/search.json?q=${searchText}`;
     
-
+    // number of result Fetching
     fetch(bookUrl)
     .then(res => res.json())
-    .then(data => displayNumberOFData(data.num_found))
+    .then(data => displayNumberOFData(data))
 
-   
-
+    // Book information Fetching
     fetch(bookUrl)
     .then(res => res.json())
     .then(data => displayBook(data.docs))
 
 })
 
+// for result
 const displayNumberOFData = numberOfData =>{
-    console.log(numberOfData);
 
-    if(numberOfData === 0){
+        // clear result Showing
+        resultFound.textContent = '';
+
+        const resultPara = document.createElement('p');
+        resultPara.innerHTML = `
+            <p class="text-center bg-dark w-25 mx-auto rounded text-white p-2">Result Found : ${numberOfData.num_found} Showing result : ${numberOfData.docs.length}</p>
+        `;
+        resultFound.appendChild(resultPara);
+
+    // Error Handling 
+    if(numberOfData.num_found === 0){
         const errorPara = document.createElement('p');
         errorPara.innerHTML = `
-            <h3>Please enter valid keyword</h3>
+            <h3 class="text-white text-center bg-danger mx-auto rounded p-3 w-50">Please enter valid keyword</h3>
         `;
         errorHandle.appendChild(errorPara);
+
+        // clear result Showing
+        resultFound.textContent = '';
     }
+    
 } 
 
 const displayBook = books => {
@@ -67,14 +100,15 @@ const displayBook = books => {
 
         // clear cover showing
         bookCover.textContent = '';
- 
+
+    // loop for every Books information
     books.forEach(book => {
         const booksDiv = document.createElement('div');
         booksDiv.classList.add('col');
         booksDiv.innerHTML = `
-                <div class="card">
+                <div class="card h-100">
                     <div class="card-body">
-                        
+                        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class= "card-img-center rounded mx-auto d-block my-2">
                         <h3 class="card-title fw-bold fs-5 ">${book.title}</h3>
                         <h4 class="text-muted">Author : ${book.author_name}</h4>
                         <h5 class="fw-light text-muted ">Publlisher : ${book.publisher}</h5>
@@ -85,15 +119,21 @@ const displayBook = books => {
                 </div>
         `;
         bookInfo.appendChild(booksDiv);
-    });   
+
+    });
+
+    // spinner stop
+    toggleSpinner('none');
+    
 }
 
-const displayDetails = async details => {
-    const coverImgUrl = `https://covers.openlibrary.org/b/id/${details}-M.jpg`;
-    loadDetails(details);
+
+const displayCoverImg = coverImg => {
+    const coverImgUrl = `https://covers.openlibrary.org/b/id/${coverImg}-M.jpg`;
+    loadCover(coverImg);
 }
 
-const loadDetails = showCover => {
+const loadCover = showCover => {
      
         // clear cover showing
         bookCover.textContent = '';
@@ -102,11 +142,9 @@ const loadDetails = showCover => {
     detailsDiv.innerHTML = `
         <div class="card mx-auto my-4 w-25">
             <img src="https://covers.openlibrary.org/b/id/${showCover}-M.jpg" class="card-img-top" alt="...">
-            <button class="w-50 btn btn-outline-info mx-auto my-2">Buy Now</button>    
+            <button class="w-50 btn btn-outline-secondary mx-auto my-2">Buy Now</button>    
             </div>
         </div>
     `;
     bookCover.appendChild(detailsDiv);
 }
-
-// <img src="https://covers.openlibrary.org/b/id/${books.cover_i}-L.jpg">
